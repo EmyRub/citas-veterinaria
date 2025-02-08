3e7ab79dbc3a5bb7efa0a0891fb70e39b26be91c
EmyRub <esme.rubi@hotmail.com>
2025-02-07 18:28:39 -0600

    performance

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div className="container mx-auto mt-20">
-        <h1 className="font-black text-5xl text-center md:w-2/3 md:max-auto">
+        <h1 className="font-black text-5xl text-center mb-12 mx-auto md:w-2/3 md:max-auto">
           Seguimiento de Pacientes
           <span className="text-indigo-700">Veterinaria</span>
         </h1>
